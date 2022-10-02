bc753e704bae772102b06c9ba6f0727c3dbb0ac2
Max <imv-1max@yandex.ru>
2022-10-02 23:11:31 +0500

    Final_CW

---
+++ CW/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+void GetRead(string[] arr)
+{
+    string[] sortarray = new string[arr.Length];
+    for(int i = 0; i < arr.Length; i++)
+    if(arr[i].Length <=3 ) 
+    { 
+     sortarray[i] = arr[i];   
+    Console.Write($"{sortarray[i]}, ");
+    }
+}
+string[] array = new string[] {"25", "gun", "food", "town", "1", "stop"};
+GetRead(array);
